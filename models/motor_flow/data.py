97afea50b15ec13a6b4e78a9a668d137f65f65f5
Aleksantari <aleksantari@gmail.com>
2026-01-21 09:46:38 -0500

    added eval set maker to motorflow

--- models/motor_flow/data.py
+++ models/motor_flow/data.py
@@ -21,3 +21,15 @@ def sample_batch_motorflow(episodes, batch_size, L=150, T=300):
         past[b] = ep[s : s + L]
         x0[b] = ep[s + L]
     return x0, past
+
+def make_fixed_rollout_valset(test_eps, num_eps=16, L=150, H=100, seed=0):
+    rng = np.random.default_rng(seed)
+    idxs = rng.choice(len(test_eps), size=min(num_eps, len(test_eps)), replace=False)
+    # each item is (seed_prefix, gt_suffix)
+    valset = []
+    for i in idxs:
+        ep = test_eps[i]              # (300,6)
+        seed_prefix = ep[:L]          # (L,6)
+        gt_suffix   = ep[L:L+H]       # (H,6)
+        valset.append((seed_prefix, gt_suffix))
+    return valset
